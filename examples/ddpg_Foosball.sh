55d5d3a4677c1bb98a86ff89596f6cb50a9be1d9
ggramazio <giusva90.subscriptions@gmail.com>
2018-11-03 18:24:28 +0100

    Update to create a large non-trained memory on server

--- examples/ddpg_Foosball.sh
+++ examples/ddpg_Foosball.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# xvfb-run -s "-screen 0 1400x900x24" python3 ddpg.py \
-python3 ddpg.py \
+# python3 ddpg.py \
+xvfb-run -s "-screen 0 1400x900x24" python3 ddpg.py \
   --env FoosballVREP_sp-v1 \
   --window_length 4 \
-  --memory_limit 500000 \
-  --nb_steps_warmup_actor 10000 \
-  --nb_steps_warmup_critic 10000 \
-  --nb_train_steps 50000 \
+  --memory_limit 1000000 \
+  --nb_steps_warmup_actor 100000 \
+  --nb_steps_warmup_critic 100000 \
+  --nb_train_steps 100000 \
   --verbose 1 \
   --batch_size 32 \
   --actor_hidden_units 16 16 16 \
   --critic_hidden_units 32 32 32 \
-  --nb_test_episodes 20 \
-  --verbose 2 \
-  --render_train
+  --nb_test_episodes 0
