9cf3958713de229f7b278a3130a93d2cbfc1ce61
ggramazio <giusva90.subscriptions@gmail.com>
2018-11-04 22:43:57 +0100

    Increase number of steps to 1000000

--- examples/ddpg_Foosball.sh
+++ examples/ddpg_Foosball.sh
@@ -7,7 +7,7 @@ xvfb-run -s "-screen 0 1400x900x24" python3 ddpg.py \
   --memory_limit 1000000 \
   --nb_steps_warmup_actor 0 \
   --nb_steps_warmup_critic 0 \
-  --nb_train_steps 100000 \
+  --nb_train_steps 1000000 \
   --verbose 1 \
   --batch_size 32 \
   --nb_test_episodes 0 \
