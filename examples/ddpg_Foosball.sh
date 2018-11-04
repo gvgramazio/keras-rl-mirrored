c41593cd7665d1297f6a6155555775ce89aafb4e
ggramazio <giusva90.subscriptions@gmail.com>
2018-11-04 22:38:53 +0100

    update only for the server

--- examples/ddpg_Foosball.sh
+++ examples/ddpg_Foosball.sh
@@ -5,9 +5,10 @@ xvfb-run -s "-screen 0 1400x900x24" python3 ddpg.py \
   --env FoosballVREP_sp-v1 \
   --window_length 4 \
   --memory_limit 1000000 \
-  --nb_steps_warmup_actor 1000 \
-  --nb_steps_warmup_critic 1000 \
+  --nb_steps_warmup_actor 0 \
+  --nb_steps_warmup_critic 0 \
   --nb_train_steps 100000 \
   --verbose 1 \
   --batch_size 32 \
-  --nb_test_episodes 0
+  --nb_test_episodes 0 \
+  --load_memory
