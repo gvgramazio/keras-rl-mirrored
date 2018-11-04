bd0ceb39a8a3dc29a0aa2e4805b3fb0e8ea37be9
ggramazio <giusva90.subscriptions@gmail.com>
2018-11-04 22:48:36 +0100

    fix dropout import

--- examples/ddpg.py
+++ examples/ddpg.py
@@ -64,7 +64,7 @@ import gym
 import gym_foosball
 
 from keras.models import Sequential, Model
-from keras.layers import Dense, Activation, Flatten, Input, Concatenate
+from keras.layers import Dense, Activation, Flatten, Input, Concatenate, Dropout
 from keras.optimizers import Adam
 
 from rl.agents import DDPGAgent
@@ -120,9 +120,9 @@ else:
 actor = Sequential()
 actor.add(Flatten(input_shape=(WINDOW_LENGHT,) + env.observation_space.shape))
 actor.add(Dense(1024, activation='relu'))
-actor.add(Dropout(0.4))
+actor.add(Dropout(0.2))
 actor.add(Dense(512, activation='relu'))
-actor.add(Dropout(0.4))
+actor.add(Dropout(0.2))
 actor.add(Dense(nb_actions))
 actor.add(Activation('sigmoid'))
 print(actor.summary())
@@ -131,10 +131,10 @@ action_input = Input(shape=(nb_actions,), name='action_input')
 observation_input = Input(shape=(WINDOW_LENGHT,) + env.observation_space.shape, name='observation_input')
 flattened_observation = Flatten()(observation_input)
 x = Dense(1024, activation='relu')(flattened_observation)
-x = Dropout(0.4)(x)
+x = Dropout(0.2)(x)
 x = Concatenate()([x, action_input])
 x = Dense(512, activation='relu')(x)
-x = Dropout(0.4)(x)
+x = Dropout(0.2)(x)
 x = Dense(1)(x)
 x = Activation('linear')(x)
 critic = Model(inputs=[action_input, observation_input], outputs=x)
